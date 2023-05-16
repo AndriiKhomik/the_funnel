1a0053afcfebe964cd9ab0c0a01ba234bdc838db
Andrii <andrii.khomik@gmail.com>
2023-05-16 15:16:32 +0300

    improve styles

--- pages/projects.js
+++ pages/projects.js
@@ -17,22 +17,19 @@ const Projects = () => {
       {projects.length === 0 && (
         <h4 className={styles.subtitle}>Please add your first project</h4>
       )}
-      <ul>
+      <ul className={styles.list}>
         {projects.length &&
           projects?.map((project, i) => (
             <li key={`${project.projectName}-${i}`} className={styles.listItem}>
               <div className={styles.projectName}>{project.projectName}</div>
-              <div className={styles.linkWrapper}>
-                Go to project page:{" "}
-                <a
-                  className={styles.link}
-                  href={`https://${project.projectUrl}`}
-                  target="_blank"
-                >
-                  {project.projectUrl}
-                </a>
-              </div>
-              <div>email: {project.email}</div>
+              <a
+                className={styles.linkWrapper}
+                href={`https://${project.projectUrl}`}
+                target="_blank"
+              >
+                Go to project page
+              </a>
+              <div>E-mail: {project.email}</div>
               <div>Project goal: {project.goal}</div>
               <div>Workers amount: {project.workers}</div>
               <div>Launch method: {project.launchMethod}</div>
